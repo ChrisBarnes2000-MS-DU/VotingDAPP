0777f4cf5b5d890b607b7e51a1dc9b0e5579ad1e
Christopher Barnes <chris.barnes.2000@me.com>
2021-06-26 00:58:02 -0700

    [Add]: Voting Smart Contract Logic & Tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,7 +8,10 @@ contract Election {
         uint voteCount;
     }
 
-    // Read/write Candidates
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
 
     // Store Candidates Count
@@ -23,5 +26,19 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
