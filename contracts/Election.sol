07aea358e545aaa4915c0f7c4a15da00c08c4153
Christopher Barnes <chris.barnes.2000@me.com>
2021-06-25 19:38:47 -0700

    [Add]: Step 2 .1 - List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
 
     // Constructor
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
